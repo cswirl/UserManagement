56d09a9a66e10b47449b993a183cc358e5c98455
cswirl <jay.angeles@protonmail.com>
2022-03-30 22:39:12 -0400

    Preparation for Initial Migration

---
+++ api/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using api.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ api/Data/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Data.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
