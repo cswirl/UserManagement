57c6e57f36feccc7a9447b7df42e4b3db79c551f
cswirl <jay.angeles@protonmail.com>
2022-04-01 01:02:46 -0400

    Automapper setup

---
+++ api/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using api.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
---
+++ api/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using api.Data.Entities;
+using api.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Helpers
+{
+    public class AutoMapperProfiles : AutoMapper.Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<RegisterDto, AppUser>();
+        }
+    }
+}
