dcbbcb4e97d3866675dcab4b96474c9275fe5259
cswirl <jay.angeles@protonmail.com>
2022-04-13 12:52:05 -0400

    Fix Identity config to work with JWT Authentication

--- api/Extensions/IdentityServiceExtension.cs
+++ api/Extensions/IdentityServiceExtension.cs
@@ -14,28 +14,27 @@ namespace api.Extensions
         public static IServiceCollection AddIdentityServices(this IServiceCollection services)
         {
 
-            services.AddIdentity<AppUser, IdentityRole>(options =>
+            services.AddIdentityCore<AppUser>(options =>
             {
                 options.Password.RequiredLength = 4;
                 options.Password.RequireLowercase = false;
                 options.Password.RequireUppercase = false;
                 options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
 
                 options.Lockout.MaxFailedAccessAttempts = 5;
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
 
                 options.User.RequireUniqueEmail = true;
-                options.SignIn.RequireConfirmedEmail = true;
+                //options.SignIn.RequireConfirmedEmail = true;
             })
-            .AddEntityFrameworkStores<AppDbContext>()
-            .AddDefaultTokenProviders();    // This will give us the Email 2FA functionality
+            .AddRoles<IdentityRole>()
+            .AddRoleManager<RoleManager<IdentityRole>>()
+            .AddSignInManager<SignInManager<AppUser>>()
+            .AddRoleValidator<RoleValidator<IdentityRole>>()
+            .AddEntityFrameworkStores<AppDbContext>();
+            //.AddDefaultTokenProviders();    // This will give us the Email 2FA functionality - hover to see more use case
 
-            //
-            //services.ConfigureApplicationCookie(options =>
-            //{
-            //    options.LoginPath = "/Login";
-            //    options.AccessDeniedPath = "/AccessDenied";
-            //});
 
             return services;
         }
